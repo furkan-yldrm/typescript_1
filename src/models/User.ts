2c3d62dac6cb9463ae765f254b04bce9530faac9
Furkan Yıldırım <yildirim.frkn@outlook.com>
2025-01-28 10:16:25 +0300

    edit

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,3 +1,4 @@
+import { Ilogin } from "../interfaces/Ilogin";
 import { UserRole } from "../utils/UserRole";
 
 export class User {
@@ -6,13 +7,14 @@ export class User {
   private id: number;
   protected username: string;
   private password: string;
-  private readonly role: string = UserRole;
+  private readonly role: UserRole = UserRole.User;
   private lastLogin: Date | null = null;
   
   constructor(username: string, password: string, role:UserRole=UserRole.User) {
     this.id = User.idCounter++;
     this.username = username;
     this.password = password;
+    this.role = role;
   } 
 
   public updateLastLogin(): void {
@@ -38,4 +40,29 @@ public getLastLogin(): Date | null {
   public getRole(): string {
     return this.role;
   }
+
+  specialPage(): void {
+    if (UserRole.Admin === this.role) {
+        console.log("Admin sayfasina hoşgeldiniz");
+    } else if (UserRole.Moderator === this.role) {
+        console.log("Moderator sayfaiına hoşgeldiniz");
+    }
+}
+
+// Remember Me
+loginUser(data: Ilogin): void {
+    console.log(`User Logged In${data.username}`);
+    if (data.rememberMe) {
+        console.log("Remember Me özelliği ile kullaniciyi artik taniyorum");
+    } else {
+        console.log("");
+    }
+}
+
+// Kullanıcı Bilgileri Göster
+public displayUserInfo(): void {
+    console.log(
+        `User Info: User ID: ${this.id} - username: ${this.username} - Roles: ${this.role}`
+    );
+}
 }
